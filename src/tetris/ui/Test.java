cc1915356e85dbaa65d9002cd3cb79a5aaafcb3f
Jason Smith <jasmit5@ilstu.edu>
2018-10-28 20:35:03 -0500

    First commit

---
+++ src/tetris/ui/Test.java
@@ -0,0 +1,15 @@
+package tetris.ui;
+
+import javax.swing.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Hello Swing");
+		frame.setSize(1200,1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.setVisible(true);
+	}
+
+}
