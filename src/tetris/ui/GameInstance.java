a436be421acaf1f9888069ba7b8c5b0107dbd7b8
Jason Smith <jasmit5@ilstu.edu>
2018-10-29 20:44:08 -0500

    Things

--- src/tetris/ui/GameInstance.java
+++ src/tetris/ui/GameInstance.java
@@ -3,25 +3,26 @@ package tetris.ui;
 import javax.swing.JFrame;
 
 public class GameInstance {
+	JFrame mainContainer; 
 	
 	public static void main(String[] args) {
 		GameInstance game = new GameInstance();
-		game.launch();
-		
+		GameInstance.launch(game);
 	}
 	
-	public void launch() {
+	public GameInstance() {
+		mainContainer = new JFrame("Hello Swing");
+		mainContainer.setSize(1200,900);
+		mainContainer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void launch(GameInstance g) {
 		// TODO eventually launch into a window where you can connect to a another player aka createHomeMenu
-		
-		JFrame frame = new JFrame("Hello Swing");
-		frame.setSize(1200,1000);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        frame.setVisible(true);
+		g.createPlayingArea();
 	}
 	
 	public void createPlayingArea() {
-		
+        this.mainContainer.setVisible(true);
 	}
 	
 	public void createHomeMenu() {}
