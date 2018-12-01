4b71c00311d5f15f0fc50fadf7c4d84767411741
Dominik Lameter <dominik@lameter.com>
2018-12-01 01:06:38 -0600

    Added server implementation

--- src/tetris/network/Server.java
+++ src/tetris/network/Server.java
@@ -1,5 +1,52 @@
 package tetris.network;
 
-public class Server {
+import java.rmi.RemoteException;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class Server implements ServerInterface {
+
+	private Connection host;
+	private Queue<Message> queue;
+	private ServerManager manager;
+	
+	public Server(Connection host, ServerManager manager) {
+		this.host = host;
+		this.manager = manager;
+		
+		queue = new ConcurrentLinkedQueue<Message>();
+	}
+	
+	@Override
+	public void register(Connection conn) throws RemoteException {
+		manager.connect(conn);
+	}
+	
+	@Override
+	public void unregister(Connection connection) {
+		manager.disconnect(connection);
+	}
+
+	@Override
+	public void send(Message message) {
+		queue.offer(message);
+	}
+
+	@Override
+	public Connection getHostConnection() {
+		return host;
+	}
+	@Override
+	public Set<Connection> getRegistered() {
+		return manager.getConnected();
+	}
+	
+	public Message receive() {
+		return queue.poll();
+	}
+	
+	public int queueSize() {
+		return queue.size();
+	}
 }
