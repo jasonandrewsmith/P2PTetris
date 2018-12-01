c99e8a33e9df28faa248bf5e4f3ea956f28765ac
Dominik Lameter <dominik@lameter.com>
2018-12-01 01:06:02 -0600

    Changed interface to resemble new ideas for interaction model

--- src/tetris/network/ServerInterface.java
+++ src/tetris/network/ServerInterface.java
@@ -1,12 +1,14 @@
 package tetris.network;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.Set;
 
 public interface ServerInterface extends Remote {
 
-	public void connect(String host, int port);
-	public void register(String host, int port);
-	public void send(Object object);
-	public Set<Connection> getRegistered();
+	public void register(Connection connection) throws RemoteException;
+	public void unregister(Connection connection) throws RemoteException;
+	public void send(Message message) throws RemoteException;
+	public Connection getHostConnection() throws RemoteException;
+	public Set<Connection> getRegistered() throws RemoteException;
 }
